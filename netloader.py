deea1f4ff6ea3ce3cde9669c779b46d21d39712b
Lorenzo Ferrante <l.ferrante@iungo.it>
2020-07-21 17:06:44 +0200

    Created develop branch

--- netloader.py
+++ netloader.py
@@ -5,9 +5,9 @@ print("Loading neural nets and classes...")
 painting_net = cv2.dnn.readNet('cfg/weights/painting.weights', 'cfg/net/painting.cfg')
 painting_classes = ['painting']
 
-people_net = cv2.dnn.readNet('cfg/weights/yolov3.weights', 'cfg/net/yolov3.cfg')
+people_net = cv2.dnn.readNet('cfg/weights/person.weights', 'cfg/net/person.cfg')
 people_classes = []
-with open('cfg/coco/coco.names') as f :
+with open('cfg/coco/person.names') as f:
     people_classes = [line.strip() for line in f.readlines()]
 
 print("Loaded")
